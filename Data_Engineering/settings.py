c2d61de91654c91ec26e3f04217f18bda0740680
malakar-rishabh <110848176+malakar-rishabh@users.noreply.github.com>
2023-04-09 18:52:13 +0530

    Confirmation mail
    here I have added a new functionality added which is when user signup successfully then a confirmation mail will be sent to that particular mail id.

--- Data_Engineering/settings.py
+++ Data_Engineering/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,5 +135,14 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+#settings for custom user model
 AUTH_USER_MODEL = "home.SiteUser"
+
+#settings for sending email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rishabhmalakar27@gmail.com'
+EMAIL_HOST_PASSWORD = 'zcktwyzptwpclaca'
+
